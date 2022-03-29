2c4cf63835cb50a2be67d34dd17c702ace959e52
MonikaAgarwal542 <84187029+MonikaAgarwal542@users.noreply.github.com>
2022-03-30 00:07:33 +0530

    Create User.java

---
+++ CaseStudy3/User.java
@@ -0,0 +1,46 @@
+package com.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private int userId;
+	private String userName;
+	private String emailId;
+	private String password;
+	private String mobileNo;
+	public User() {}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+}
